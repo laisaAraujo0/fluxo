805f0e84922bfd149f8a030255aedcbbc550b3d5
laisaAraujo0 <araujolaisa515@gmail.com>
2025-10-22 18:20:07 -0300

    SonarQubeCloud

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 export default {
-  testEnvironment: 'node',
-  transform: {}, 
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text', 'text-summary'],
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
 };
-
